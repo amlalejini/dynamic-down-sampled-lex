cd9150636de5fec351411995aa29e49b85ba0b34
Alexander Lalejini <amlalejini@gmail.com>
2022-05-11 14:15:18 -0400

    draft 2022-05-11 experiment

---
+++ experiments/2022-05-11-init/hpc/job-gen.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+REPLICATES=30
+EXP_SLUG=2022-05-11-init
+ACCOUNT=devolab
+SEED_OFFSET=1000
+JOB_TIME=96:00:00
+JOB_MEM=16G
+
+SCRATCH_EXP_DIR=/mnt/scratch/lalejini/data/dynamic-ds-lex
+REPO_DIR=/mnt/home/lalejini/devo_ws/dynamic-down-sampled-lex
+HOME_EXP_DIR=${REPO_DIR}/experiments
+
+DATA_DIR=${SCRATCH_EXP_DIR}/${EXP_SLUG}
+JOB_DIR=${SCRATCH_EXP_DIR}/${EXP_SLUG}/jobs
+CONFIG_DIR=${HOME_EXP_DIR}/${EXP_SLUG}/hpc/config
+
+python3 gen-sub.py --time_request ${JOB_TIME} --mem ${JOB_MEM} --data_dir ${DATA_DIR} --config_dir ${CONFIG_DIR} --repo_dir ${REPO_DIR} --replicates ${REPLICATES} --job_dir ${JOB_DIR} --account ${ACCOUNT} --seed_offset ${SEED_OFFSET}
