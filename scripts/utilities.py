ce1ef5581df3a3195a7835b1baa9ebf46a2912cc
Alexander Lalejini <amlalejini@gmail.com>
2022-05-11 14:52:14 -0400

    add utilities script

---
+++ scripts/utilities.py
@@ -0,0 +1,12 @@
+import errno, os, csv
+
+def mkdir_p(path):
+    """
+    This is functionally equivalent to the mkdir -p [fname] bash command
+    """
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
